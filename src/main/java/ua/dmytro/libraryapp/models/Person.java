dad02dc757ded3e7001adf8878b11a4cdecc8283
Snakerser <35500247+Snakerser@users.noreply.github.com>
2023-03-02 20:31:34 +0200

    Add files via upload

---
+++ src/main/java/ua/dmytro/libraryapp/models/Person.java
@@ -0,0 +1,51 @@
+package ua.dmytro.libraryapp.models;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @NotNull(message = "Age should not be empty")
+    private int age;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
